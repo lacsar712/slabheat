6ba2f76fb4102c856471273cd41816cf68c0c8a8
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-27 13:39:31 +0800

    fix: apply model patch, add acceptance test

--- internal/app/calibrate.go
+++ internal/app/calibrate.go
@@ -9,15 +9,13 @@ var CalibrateProbe func(ctx context.Context) error
 
 func (a *App) Calibrate(ctx context.Context, holder string) error {
 	now := a.clk.Now()
-	_, err := a.interlock.Leases().Acquire(a.cfg.UnitID, holder, now)
-	if err != nil {
-		return err
-	}
-	if CalibrateProbe != nil {
-		if err := CalibrateProbe(ctx); err != nil {
-			return fmt.Errorf("calibrate: %w", err)
+	return a.interlock.Leases().WithLease(ctx, a.cfg.UnitID, holder, now, func(ctx context.Context) error {
+		if CalibrateProbe != nil {
+			if err := CalibrateProbe(ctx); err != nil {
+				return fmt.Errorf("calibrate: %w", err)
+			}
 		}
-	}
-	a.journalEvent("calibrate", fmt.Sprintf("{\"holder\":\"%s\"}", holder))
-	return nil
+		a.journalEvent("calibrate", fmt.Sprintf("{\"holder\":\"%s\"}", holder))
+		return nil
+	})
 }
